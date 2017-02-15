942b7edc14edbe3cb0f2c391e8242ea172158e5c
dicksonmuli <dicksonmully6@gmail.com>
2017-02-15 23:19:57 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+  $("h4#item1").click(function() {
+    $("p.class-p1").toggle();
+  });
+  $("h4#item2").click(function() {
+    $("p.class-p2").toggle();
+  });
+  $("h4#item3").click(function() {
+    $("p.class-p3").toggle();
+  });
+  $("h4#item4").click(function() {
+    $("p.class-p4").toggle();
+  });
+  $("h4#item5").click(function() {
+    $("p.class-p5").toggle();
+  });
+  $("h4#item6").click(function() {
+    $("p.class-p6").toggle();
+  });
+  $("h4#item7").click(function() {
+    $("p.class-p7").toggle();
+  });
+  $("h4#item8").click(function() {
+    $("p.class-p8").toggle();
+  });
+  $("h4#item9").click(function() {
+    $("p.class-p9").toggle();
+  });
+  $("h4#item10").click(function() {
+    $("p.class-p10").toggle();
+  });
+  $("h4#item11").click(function() {
+    $("p.class-p11").toggle();
+  });
+  $("h4#item12").click(function() {
+    $("p.class-p12").toggle();
+  });
+  $("h4#item13").click(function() {
+    $("p.class-p13").toggle();
+  });
+  $("h4#item14").click(function() {
+    $("p.class-p14").toggle();
+  });
+  $("h4#item15").click(function() {
+    $("p.class-p15").toggle();
+  });
+  $("h4#item16").click(function() {
+    $("p.class-p16").toggle();
+  });
+  $("h4#item17").click(function() {
+    $("p.class-p17").toggle();
+  });
+  $("h4#item18").click(function() {
+    $("p.class-p18").toggle();
+  });
+  $("h4#item19").click(function() {
+    $("p.class-p19").toggle();
+  });
+})
